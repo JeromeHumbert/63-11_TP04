905a4d3a7abbd25bc2abe7ba17979ef2ad9dba15
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2021-11-30 15:24:44 +0100

    Add files via upload

---
+++ tp04_ex1.py
@@ -0,0 +1,10 @@
+# Constantes
+CATALOGUE_CHAUSSURES: list = [['Asics Gel 2000', 42, 119],
+                        ['Asics Gel 2000', 39, 119],
+                        ['Mizuno Wave rider', 38, 129],
+                        ['Nike Air zoom', 42, 125],
+                        ['Mizuno Wave plus', 39, 83.40],
+                        ['Mizuno Wave plus', 40, 83.40],
+                        ['Mizuno Wave plus', 41, 83.40],
+                        ['Merrell Poseidon', 39, 118.40]]
+
